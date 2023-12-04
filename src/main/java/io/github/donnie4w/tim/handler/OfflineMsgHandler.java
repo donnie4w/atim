e875d0a1a68b4d80627b53368a158ccf0cf9f3ba
donnie <donnie4w@gmail.com>
2023-12-04 22:12:43 +0800

    Create OfflineMsgHandler.java

---
+++ src/main/java/io/github/donnie4w/tim/handler/OfflineMsgHandler.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) , donnie <donnie4w@gmail.com>
+ * All rights reserved.
+ * https://github.com/donnie4w/tim
+ * https://githuc.com/donnie4w/atim
+ *
+ * Use of this source code is governed by a MIT-style license that can be
+ * found in the LICENSE file
+ */
+package io.github.donnie4w.tim.handler;
+
+import io.github.donnie4w.tim.handle.TimException;
+import io.github.donnie4w.tim.stub.TimMessageList;
+
+public interface OfflineMsgHandler {
+    public  void run(TimMessageList tml) throws TimException;
+}
