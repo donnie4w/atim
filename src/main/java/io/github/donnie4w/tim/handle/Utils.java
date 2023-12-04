4b2d526de3afef41354c9e7ac0688e88972de048
donnie <donnie4w@gmail.com>
2023-12-04 22:12:15 +0800

    Create Utils.java

---
+++ src/main/java/io/github/donnie4w/tim/handle/Utils.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) , donnie <donnie4w@gmail.com>
+ * All rights reserved.
+ * https://github.com/donnie4w/tim
+ * https://githuc.com/donnie4w/atim
+ *
+ * Use of this source code is governed by a MIT-style license that can be
+ * found in the LICENSE file
+ */
+package io.github.donnie4w.tim.handle;
+
+import org.apache.thrift.TSerializable;
+import org.apache.thrift.protocol.TCompactProtocol;
+import org.apache.thrift.transport.TMemoryBuffer;
+import org.jetbrains.annotations.NotNull;
+import java.nio.charset.StandardCharsets;
+
+public class Utils {
+    static byte[] tEncode(@NotNull TSerializable ts) throws TimException {
+        try {
+            TMemoryBuffer tmb = new TMemoryBuffer(0);
+            ts.write(new TCompactProtocol(tmb));
+            return tmb.getArray();
+        } catch (Exception e) {
+            throw new TimException(e);
+        }
+    }
+
+    static <T extends TSerializable> T tDecode(@NotNull byte[] bs, T ts) throws TimException {
+        try {
+            TMemoryBuffer tmb = new TMemoryBuffer(1024);
+            tmb.write(bs);
+            ts.read(new TCompactProtocol(tmb));
+            return ts;
+        } catch (Exception e) {
+            throw new TimException(e);
+        }
+    }
+
+    static String string(@NotNull byte[] bs) {
+        return new String(bs, StandardCharsets.ISO_8859_1);
+    }
+
+    static byte[] bytes(@NotNull String s) {
+        return s.getBytes(StandardCharsets.ISO_8859_1);
+    }
+
+    static boolean isBlank(String v) {
+        return null == v || "".equals(v.trim());
+    }
+
+}
