ecb4a94afec17a310981c336bd152d79f3926b81
donnie <donnie4w@gmail.com>
2023-12-04 22:12:33 +0800

    Create OfflinemsgEndHandler.java

---
+++ src/main/java/io/github/donnie4w/tim/handler/OfflinemsgEndHandler.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) , donnie <donnie4w@gmail.com>
+ * All rights reserved.
+ * https://github.com/donnie4w/tim
+ * https://githuc.com/donnie4w/atim
+ *
+ * Use of this source code is governed by a MIT-style license that can be
+ * found in the LICENSE file
+ */
+package io.github.donnie4w.tim.handler;
+
+import io.github.donnie4w.tim.handle.TimException;
+
+public interface OfflinemsgEndHandler {
+    public void run() throws TimException;
+}
