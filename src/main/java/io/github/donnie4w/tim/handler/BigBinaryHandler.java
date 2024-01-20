49ccf90909a76ee19b22d3251bd150a354b6c3f1
donnie4w <donnie4w@gmail.com>
2024-01-20 15:48:53 +0800

    Create BigBinaryHandler.java

---
+++ src/main/java/io/github/donnie4w/tim/handler/BigBinaryHandler.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) , donnie <donnie4w@gmail.com>
+ * All rights reserved.
+ * https://github.com/donnie4w/tim
+ * https://githuc.com/donnie4w/atim
+ *
+ * Use of this source code is governed by a MIT-style license that can be
+ * found in the LICENSE file
+ */
+package io.github.donnie4w.tim.handler;
+
+import io.github.donnie4w.tim.handle.TimException;
+import io.github.donnie4w.tim.stub.TimStream;
+
+public interface BigBinaryHandler {
+    public  void run(byte[] ts) throws TimException;
+}
